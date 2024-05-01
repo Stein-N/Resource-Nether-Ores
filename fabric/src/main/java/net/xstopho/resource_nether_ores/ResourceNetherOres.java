d9016d093ae4469080e9e5cd564c3045feb1aa13
Stein-N <nico-stein-98@outlook.de>
2024-05-01 11:30:55 +0200

    fabric: adding Biome Modification so Ores get Generated

--- fabric/src/main/java/net/xstopho/resource_nether_ores/ResourceNetherOres.java
+++ fabric/src/main/java/net/xstopho/resource_nether_ores/ResourceNetherOres.java
@@ -3,11 +3,14 @@ package net.xstopho.resource_nether_ores;
 import net.fabricmc.api.ModInitializer;
 import net.xstopho.resource_nether_ores.registries.BlockRegistry;
 import net.xstopho.resource_nether_ores.registries.CreativeTabRegistry;
+import net.xstopho.resource_nether_ores.worldgen.OreBiomeModifications;
 
 public class ResourceNetherOres implements ModInitializer {
     @Override
     public void onInitialize() {
         BlockRegistry.init();
         CreativeTabRegistry.init();
+
+        OreBiomeModifications.init();
     }
 }
