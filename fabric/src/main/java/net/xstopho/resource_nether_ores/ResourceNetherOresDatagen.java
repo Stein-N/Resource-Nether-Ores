a50bef0602d4ccc650c9e62f6d67afa7373bbde4
Stein-N <nico-stein-98@outlook.de>
2024-05-01 11:23:04 +0200

    fabric: adding WorldGenProv

--- fabric/src/main/java/net/xstopho/resource_nether_ores/ResourceNetherOresDatagen.java
+++ fabric/src/main/java/net/xstopho/resource_nether_ores/ResourceNetherOresDatagen.java
@@ -2,8 +2,13 @@ package net.xstopho.resource_nether_ores;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
 import net.xstopho.resource_nether_ores.datagen.BlockLootProv;
 import net.xstopho.resource_nether_ores.datagen.ModelProv;
+import net.xstopho.resource_nether_ores.datagen.WorldGenProv;
+import net.xstopho.resource_nether_ores.worldgen.OreConfiguredFeatures;
+import net.xstopho.resource_nether_ores.worldgen.OrePlacedFeatures;
 
 public class ResourceNetherOresDatagen implements DataGeneratorEntrypoint {
     @Override
@@ -12,5 +17,12 @@ public class ResourceNetherOresDatagen implements DataGeneratorEntrypoint {
 
         pack.addProvider(ModelProv::new);
         pack.addProvider(BlockLootProv::new);
+        pack.addProvider(WorldGenProv::new);
+    }
+
+    @Override
+    public void buildRegistry(RegistrySetBuilder registryBuilder) {
+        registryBuilder.add(Registries.CONFIGURED_FEATURE, OreConfiguredFeatures::bootstrap);
+        registryBuilder.add(Registries.PLACED_FEATURE, OrePlacedFeatures::bootstrap);
     }
 }
