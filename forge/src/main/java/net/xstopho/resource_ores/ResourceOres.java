1dfb4c2e5c1c42480075b47aa3548edf72472227
Stein-N <nico-stein-98@outlook.de>
2024-05-01 10:37:58 +0200

    fabric: fixing startup issues

--- forge/src/main/java/net/xstopho/resource_ores/ResourceOres.java
+++ forge/src/main/java/net/xstopho/resource_ores/ResourceOres.java
@@ -1,9 +1,7 @@
 package net.xstopho.resource_ores;
 
 import net.minecraftforge.fml.common.Mod;
-import net.xstopho.resource_ores.registries.BlockRegistry;
-import net.xstopho.resource_ores.registries.ItemGroupRegistry;
-import net.xstopho.resource_ores.registries.ItemRegistry;
+import net.xstopho.resource_nether_ores.OreConstants;
 
 @Mod(OreConstants.MOD_ID)
 public class ResourceOres {
