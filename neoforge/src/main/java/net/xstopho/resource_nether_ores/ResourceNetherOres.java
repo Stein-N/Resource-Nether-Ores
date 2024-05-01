ecb8213a32ca668c7e4b1edc0c1b516a011e0b98
Stein-N <nico-stein-98@outlook.de>
2024-05-01 10:34:08 +0200

    project: initial setup

---
+++ neoforge/src/main/java/net/xstopho/resource_nether_ores/ResourceNetherOres.java
@@ -0,0 +1,12 @@
+package net.xstopho.resource_nether_ores;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(OreConstants.MOD_ID)
+public class ResourceNetherOres {
+    
+    public ResourceNetherOres(IEventBus bus) {
+
+    }
+}
