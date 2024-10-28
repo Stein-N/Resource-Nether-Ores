b2b63e6b85ce331497c8509113b958f9676d3ffd
Stein-N <nico-stein-98@outlook.de>
2024-10-28 17:05:53 +0100

    update to 1.21.3

--- common/src/main/java/net/xstopho/resource_nether_ores/registries/BlockRegistry.java
+++ common/src/main/java/net/xstopho/resource_nether_ores/registries/BlockRegistry.java
@@ -1,6 +1,9 @@
 package net.xstopho.resource_nether_ores.registries;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
@@ -14,32 +17,62 @@ import net.xstopho.resource_nether_ores.OreConstants;
 import net.xstopho.resourcelibrary.registration.RegistryObject;
 import net.xstopho.resourcelibrary.registration.RegistryProvider;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class BlockRegistry {
 
-    public static final RegistryProvider<Block> BLOCKS = RegistryProvider.get(Registries.BLOCK, OreConstants.MOD_ID);
-    private static final RegistryProvider<Item> ITEMS = RegistryProvider.get(Registries.ITEM, OreConstants.MOD_ID);
+    public static final RegistryProvider<Block> BLOCKS = RegistryProvider.get(OreConstants.MOD_ID, BuiltInRegistries.BLOCK);
+    private static final RegistryProvider<Item> ITEMS = RegistryProvider.get(OreConstants.MOD_ID, BuiltInRegistries.ITEM);
+
 
     public static final RegistryObject<Block> NETHER_COAL_ORE = register("nether_coal_ore",
-            () -> new DropExperienceBlock(UniformInt.of(1, 3), BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE)));
-    public static final RegistryObject<Block> NETHER_COPPER_ORE = register("nether_copper_ore",
-            () -> new DropExperienceBlock(ConstantInt.of(0), BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_ORE)));
-    public static final RegistryObject<Block> NETHER_IRON_ORE = register("nether_iron_ore",
-            () -> new DropExperienceBlock(ConstantInt.of(0), BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE)));
-    public static final RegistryObject<Block> NETHER_DIAMOND_ORE = register("nether_diamond_ore",
-            () -> new DropExperienceBlock(UniformInt.of(1, 7), BlockBehaviour.Properties.ofFullCopy(Blocks.DIAMOND_ORE)));
-    public static final RegistryObject<Block> NETHER_EMERALD_ORE = register("nether_emerald_ore",
-            () -> new DropExperienceBlock(UniformInt.of(1, 7), BlockBehaviour.Properties.ofFullCopy(Blocks.EMERALD_ORE)));
-    public static final RegistryObject<Block> NETHER_LAPIS_ORE = register("nether_lapis_ore",
-            () -> new DropExperienceBlock(UniformInt.of(1, 5), BlockBehaviour.Properties.ofFullCopy(Blocks.LAPIS_ORE)));
-    public static final RegistryObject<Block> NETHER_REDSTONE_ORE = register("nether_redstone_ore",
-            () -> new RedStoneOreBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.REDSTONE_ORE)));
+            properties -> new DropExperienceBlock(ConstantInt.of(0), properties),
+            BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
 
-    private static RegistryObject<Block> register(String id, Supplier<Block> block) {
-        RegistryObject<Block> toReturn = BLOCKS.register(id, block);
-        ITEMS.register(id, () -> new BlockItem(toReturn.get(), new Item.Properties()));
+    public static final RegistryObject<Block> NETHER_COPPER_ORE = register("nether_copper_ore",
+            properties -> new DropExperienceBlock(ConstantInt.of(0), properties),
+            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_ORE));
+
+    public static final RegistryObject<Block> NETHER_IRON_ORE = register("nether_iron_ore",
+            properties -> new DropExperienceBlock(ConstantInt.of(0), properties),
+            BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE));
+
+    public static final RegistryObject<Block> NETHER_DIAMOND_ORE = register("nether_diamond_ore",
+            properties -> new DropExperienceBlock(UniformInt.of(1, 7), properties),
+            BlockBehaviour.Properties.ofFullCopy(Blocks.DIAMOND_ORE));
+
+    public static final RegistryObject<Block> NETHER_EMERALD_ORE = register("nether_emerald_ore",
+            properties -> new DropExperienceBlock(UniformInt.of(1, 7), properties),
+            BlockBehaviour.Properties.ofFullCopy(Blocks.EMERALD_ORE));
+
+    public static final RegistryObject<Block> NETHER_LAPIS_ORE = register("nether_lapis_ore",
+            properties -> new DropExperienceBlock(UniformInt.of(1, 5), properties),
+            BlockBehaviour.Properties.ofFullCopy(Blocks.LAPIS_ORE));
+
+    public static final RegistryObject<Block> NETHER_REDSTONE_ORE = register("nether_redstone_ore",
+            RedStoneOreBlock::new, BlockBehaviour.Properties.ofFullCopy(Blocks.REDSTONE_ORE));
+
+
+
+
+    private static RegistryObject<Block> register(String id, Function<BlockBehaviour.Properties, Block> function, BlockBehaviour.Properties blockBehavior) {
+        RegistryObject<Block> toReturn = BLOCKS.register(id, () -> function.apply(blockBehavior.setId(createBlockKey(id))));
+        register(id, properties -> new BlockItem(toReturn.get(), properties));
+
         return toReturn;
+    }
+
+    private static void register(String id, Function<Item.Properties, Item> function) {
+        ITEMS.register(id, () -> function.apply(new Item.Properties().setId(createItemKey(id))));
+    }
+
+    private static ResourceKey<Block> createBlockKey(String id) {
+        return ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(OreConstants.MOD_ID, id));
+    }
+
+    private static ResourceKey<Item> createItemKey(String id) {
+        return ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(OreConstants.MOD_ID, id));
     }
 
     public static void init() {}
