ea7b840979081ab29634faea2459fe4e3fe0a27d
Stein-N <nico-stein-98@outlook.de>
2024-12-05 14:34:38 +0100

    use correct ore tag

--- common/src/main/java/net/xstopho/resource_nether_ores/tags/OreTags.java
+++ common/src/main/java/net/xstopho/resource_nether_ores/tags/OreTags.java
@@ -7,11 +7,11 @@ import net.xstopho.resourcelibrary.util.TagUtil;
 public class OreTags {
 
     public static final TagKey<Item> ORES = TagUtil.createItemTag("ores");
-    public static final TagKey<Item> COAL_ORE = TagUtil.createItemTag("ore/coal");
-    public static final TagKey<Item> COPPER_ORE = TagUtil.createItemTag("ore/copper");
-    public static final TagKey<Item> IRON_ORE = TagUtil.createItemTag("ore/iron");
-    public static final TagKey<Item> DIAMOND_ORE = TagUtil.createItemTag("ore/diamond");
-    public static final TagKey<Item> EMERALD_ORE = TagUtil.createItemTag("ore/emerald");
-    public static final TagKey<Item> LAPIS_ORE = TagUtil.createItemTag("ore/lapis");
-    public static final TagKey<Item> REDSTONE_ORE = TagUtil.createItemTag("ore/redstone");
+    public static final TagKey<Item> COAL_ORE = TagUtil.createItemTag("ores/coal");
+    public static final TagKey<Item> COPPER_ORE = TagUtil.createItemTag("ores/copper");
+    public static final TagKey<Item> IRON_ORE = TagUtil.createItemTag("ores/iron");
+    public static final TagKey<Item> DIAMOND_ORE = TagUtil.createItemTag("ores/diamond");
+    public static final TagKey<Item> EMERALD_ORE = TagUtil.createItemTag("ores/emerald");
+    public static final TagKey<Item> LAPIS_ORE = TagUtil.createItemTag("ores/lapis");
+    public static final TagKey<Item> REDSTONE_ORE = TagUtil.createItemTag("ores/redstone");
 }
